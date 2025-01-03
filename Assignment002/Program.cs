7410afa18a309e2c4195772a0f98ab361a2c7a2f
reemgaber213@gmail.com <reemgaber213@gmail.com>
2025-01-03 14:30:56 +0200

    Add project files.

---
+++ Assignment002/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
